ed4f2b0218bff57cc68441541eaffb9908cf0912
mltaarnskov <mltaarnskov@gmail.com>
2019-06-18 22:15:57 -0400

    javascriptchange to make gif hover

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-$(".main").scroll(function() {
-  $(this)
-    .find(".sticky")
-    .css("left", $(this).scrollLeft());
+var tid = null;
+$(window).scroll(function() {
+  if (tid) return;
+  tid = setTimeout(function() {
+    tid = null;
+    var el = document.documentElement.querySelector('.absolutesticky');
+    var r = el.getBoundingClientRect();
+    if (r.left <= 0 && r.right >= window.innerWidth) {
+      el.classList.add('active');
+    } else {
+      el.classList.remove('active');
+    }
+  }, 100);
 });
